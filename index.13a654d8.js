82e66dea80ef8edb768c5b36c79dc1fde9d0a700
Ingvar789 <57942756+Ingvar789@users.noreply.github.com>
2022-11-02 09:09:36 +0000

    Deploying to gh-pages from @ Ingvar789/food-delivery-project@1685af7ee3f9fb9d8d769ad9c9e467c8652a2b84 🚀

---
+++ index.13a654d8.js
@@ -0,0 +1,2 @@
+var swiper=new Swiper(".mySwiper",{cssMode:!0,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},pagination:{el:".swiper-pagination"},mousewheel:!0,keyboard:!0});
+//# sourceMappingURL=index.13a654d8.js.map
