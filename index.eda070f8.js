7856466750a80ea04be40d0555bab6314aba8c11
Ingvar789 <57942756+Ingvar789@users.noreply.github.com>
2022-11-08 09:32:36 +0000

    Deploying to gh-pages from @ Ingvar789/food-delivery-project@e2a57e309d97910be8c626a196a830b5f2763bc8 🚀

---
+++ index.eda070f8.js
@@ -0,0 +1,2 @@
+function validateForm(){console.log("problem");var o=document.forms.orderForm.emptyForm.value;if(null==o||""==o)return document.querySelector("[submitButton]").disp,!1}
+//# sourceMappingURL=index.eda070f8.js.map
