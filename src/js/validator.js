e2a57e309d97910be8c626a196a830b5f2763bc8
Ihor Bodianchuk <ihordobruy@gmail.com>
2022-11-08 11:31:09 +0200

    added validation

---
+++ src/js/validator.js
@@ -0,0 +1,10 @@
+function validateForm() {
+console.log("problem");
+var x=document.forms["orderForm"]["emptyForm"].value;
+if (x==null || x=="")
+  {
+    var submitButton = document.querySelector("[submitButton]");
+    submitButton.disp
+  return false;
+  }
+    }
